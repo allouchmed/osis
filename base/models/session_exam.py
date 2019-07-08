f284b691ea80d52c5a063d179184ac3f5292a166
Ndizera Eddy <eddyndizera@gmail.com>
2019-07-08 15:19:28 +0200

    [Score migration] Migration files
    	new file:   assessments/migrations/0006_scoresheetaddress_education_group_year.py
    	new file:   base/migrations/0463_sessionexam_education_group_year.py
    	modified:   base/models/session_exam.py

--- base/models/session_exam.py
+++ base/models/session_exam.py
@@ -40,8 +40,7 @@ class SessionExam(models.Model):
     changed = models.DateTimeField(null=True, auto_now=True)
     number_session = models.IntegerField(choices=number_session.NUMBERS_SESSION)
     learning_unit_year = models.ForeignKey('LearningUnitYear', on_delete=models.CASCADE)
-    education_group_year = models.ForeignKey('EducationGroupYear', blank=True, null=True, on_delete=models.CASCADE,
-                                             null=True)
+    education_group_year = models.ForeignKey('EducationGroupYear', blank=True, null=True, on_delete=models.CASCADE)
     offer_year = models.ForeignKey('OfferYear', blank=True, null=True, on_delete=models.CASCADE)
     progress = None
 
