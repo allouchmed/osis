7714b135d1e3e8c0dc53ea67fc85be54c64c9723
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2020-01-23 13:27:41 +0100

    hotfix/OSIS-4114 debug code for failing test

--- base/forms/learning_unit/edition.py
+++ base/forms/learning_unit/edition.py
@@ -77,7 +77,7 @@ class LearningUnitEndDateForm(forms.Form):
         if min_year > max_year:
             raise ValueError('Learning_unit {} cannot be modify'.format(self.learning_unit))
 
-        return AcademicYear.objects.min_max_years(min_year, max_year.year)
+        return AcademicYear.objects.min_max_years(min_year, max_year)
 
     def save(self, update_learning_unit_year=True):
         return edit_learning_unit_end_date(self.learning_unit, self.cleaned_data['academic_year'],
